203ba396d0f2f571d6e0d3e9a63e97a46d012296
saul <saul@example.com>
2014-06-16 00:16:39 +0200

    scales with window height

--- src/com/rodriguez/saul/flightgearpfd/MFD777View.java
+++ src/com/rodriguez/saul/flightgearpfd/MFD777View.java
@@ -20,6 +20,7 @@ public class MFD777View extends SurfaceView implements SurfaceHolder.Callback {
 	int mheight;	
 	int centerx;
 	int centery;
+	float scaleFactor;
 	
 	
 	float horizontRollAngle;
@@ -51,16 +52,17 @@ public class MFD777View extends SurfaceView implements SurfaceHolder.Callback {
 		vsMatrix = new Matrix();
 		marksMatrix = new Matrix();
 		
+		scaleFactor = (float)0.5;
 		mask = BitmapFactory.decodeResource(getResources(),R.drawable.mask);
 		horizont = BitmapFactory.decodeResource(getResources(), R.drawable.horizon);
 		vs = BitmapFactory.decodeResource(getResources(), R.drawable.vs);
 		marks = BitmapFactory.decodeResource(getResources(), R.drawable.speed_altitude);
 				
-		horizontRollAngle = 0;
-		horizontPitchAngle = 0;
-		speed = 0;
-		altitude = 0;
-		verticalSpeed = 0;
+		horizontRollAngle = 45;
+		horizontPitchAngle = 10;
+		speed = 200;
+		altitude = 12450;
+		verticalSpeed = 500;
 		
 	}
 
@@ -85,6 +87,12 @@ public class MFD777View extends SurfaceView implements SurfaceHolder.Callback {
 		centerx = mwidth/2;
 		centery = mheight/2;
 		
+		//Calculate the scale factor
+		int maskWidth = mask.getHeight();
+		
+		scaleFactor = (float)(mheight)/(float)maskWidth;
+		
+		//Draw the view
 		draw();
 		
 				
@@ -101,41 +109,29 @@ public class MFD777View extends SurfaceView implements SurfaceHolder.Callback {
 		//Prepare the mask
 		maskMatrix.reset();
 		maskMatrix.postTranslate(-mask.getWidth()/2, -mask.getHeight()/2 );
+		maskMatrix.postScale(scaleFactor, scaleFactor);
         maskMatrix.postTranslate(centerx, centery);
         
         //Prepare the artificial horizont
         horizontMatrix.reset();
         horizontMatrix.postTranslate(-horizont.getWidth()/2, (-horizont.getHeight()/2 + (float)calculatePitchshift()) );
         horizontMatrix.postRotate(horizontRollAngle);
+        horizontMatrix.postScale(scaleFactor, scaleFactor);
         horizontMatrix.postTranslate(centerx, centery);
+        
         
         //Prepare vertical speed and gray background
         vsMatrix.reset();
         vsMatrix.postTranslate(-vs.getWidth()/2, -vs.getHeight()/2);
-        vsMatrix.postTranslate(centerx+44, centery-10);
+        vsMatrix.postScale(scaleFactor, scaleFactor);
+        vsMatrix.postTranslate(centerx+(int)(44*scaleFactor), centery-(int)(10*scaleFactor));
         
         //Prepare marks, speed & altitude boxes
         marksMatrix.reset();
         marksMatrix.postTranslate(-marks.getWidth()/2, -marks.getHeight()/2);
-        marksMatrix.postTranslate(centerx+13, centery);
-                
-		
-		/*
-				Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-			 	Canvas canvas = surfaceHolder.lockCanvas();
-			    canvas.drawColor(Color.BLACK);
-			    paint.setColor(Color.BLUE);
-			    canvas.drawCircle(300, 200, 50, paint);
-			    surfaceHolder.unlockCanvasAndPost(canvas);
-		*/
-		
-		
-		
-		//Create background bitmap
-		//Bitmap bg = Bitmap.createBitmap(mwidth, mheight, Bitmap.Config.ARGB_8888);
-		
-		//Canvas bgcanvas = new Canvas(bg); 
-        //bgcanvas.drawColor(Color.BLACK);
+        marksMatrix.postScale(scaleFactor, scaleFactor);
+        marksMatrix.postTranslate(centerx+(int)(13*scaleFactor), centery);
+        
         
 		//Lock the canvas and start drawin
         Canvas canvas = surfaceHolder.lockCanvas();
@@ -145,25 +141,22 @@ public class MFD777View extends SurfaceView implements SurfaceHolder.Callback {
         paint.setFilterBitmap(true);
         paint.setDither(true);
         //paint.setColor(Color.BLUE);
-        canvas.drawColor(Color.WHITE);
-        canvas.drawCircle(300, 200, 50, paint);
+        canvas.drawColor(Color.WHITE);       
         canvas.drawBitmap(horizont, horizontMatrix, paint);
         canvas.drawBitmap(vs, vsMatrix, null);
         drawAltitudeLine(canvas);
         drawSpeedLine(canvas);
         
         
-        canvas.drawBitmap(mask,maskMatrix,null);
-        canvas.drawBitmap(marks, marksMatrix, null);
+        canvas.drawBitmap(mask,maskMatrix,paint);
+        canvas.drawBitmap(marks, marksMatrix, paint);
         
         paint.setColor(Color.WHITE);
-        paint.setTextSize(35);
-        canvas.drawText(String.format("%d", (int)speed), (centerx - 380), centery + 10, paint);
-        canvas.drawText(String.format("%d", (int)altitude), (centerx + 310), centery + 10, paint);
+        paint.setTextSize((int)(35*scaleFactor));
+        canvas.drawText(String.format("%d", (int)speed), (centerx - (int)(380*scaleFactor)), centery + (int)(10*scaleFactor), paint);
+        canvas.drawText(String.format("%d", (int)altitude), (centerx + (int)(310*scaleFactor)), centery + (int)(10*scaleFactor), paint);
         
         drawVerticalSpeed(canvas);
-        //canvas.drawTe
-        
         
         surfaceHolder.unlockCanvasAndPost(canvas);
 
@@ -182,19 +175,19 @@ public class MFD777View extends SurfaceView implements SurfaceHolder.Callback {
 		int offsetx;
 		Paint paint;
 		final float verticalPitchScale = (float) (100./200); // 100pixels/200 feet = 0.5 pixels/ft
-		offsetx = centerx + 273; //Border
+		offsetx = centerx + (int)(273*scaleFactor); //Border
 		
 		paint = new Paint();
 		paint.setAntiAlias(true);
 		paint.setDither(true);
 		paint.setColor(Color.WHITE);
-		paint.setTextSize(30);
+		paint.setTextSize((int)(30*scaleFactor));
 		paint.setStrokeWidth(2);
 		
 		//Calculate first horizontal line
 		int rest = (int)(altitude)%200; //residual to 1st lower mark in ft
 		int altitude1 = (int)(altitude) - rest; // altitude in feet 1st lower mark
-		int offset1 = (int) (rest*verticalPitchScale);
+		int offset1 = (int) (rest*(verticalPitchScale*scaleFactor));
 		int y1 = centery + offset1;
 		
 		int y[] = new int[6];
@@ -202,17 +195,17 @@ public class MFD777View extends SurfaceView implements SurfaceHolder.Callback {
 		
 		//Marks with text
 		for (int i = 0; i<6; i++){
-			y[i] = y1 - 300 + i*100;
-			alt[i] = altitude1 + 600 - i*200;
-			canvas.drawLine(offsetx, y[i], offsetx+30, y[i], paint);
-			canvas.drawText(String.format("%d", alt[i]), offsetx + 30, y[i]+10, paint);
+			y[i] = y1 - (int)((300 - i*100)*scaleFactor);
+			alt[i] = altitude1 + (600 - i*200);
+			canvas.drawLine(offsetx, y[i], offsetx+(int)(30*scaleFactor), y[i], paint);
+			canvas.drawText(String.format("%d", alt[i]), offsetx + (int)(30*scaleFactor), y[i]+(int)(10*scaleFactor), paint);
 			
 		}
 		
 		//Marks without text
 		for (int i = 0; i<6; i++){
-			y[i] = y1 - 350 + i*100; 
-			canvas.drawLine(offsetx, y[i], offsetx+30, y[i], paint);			
+			y[i] = y1 - (int)((350 - i*100)*scaleFactor); 
+			canvas.drawLine(offsetx, y[i], offsetx+(int)(30*scaleFactor), y[i], paint);			
 		}
 	}
 		
@@ -220,19 +213,19 @@ public class MFD777View extends SurfaceView implements SurfaceHolder.Callback {
 			int offsetx;
 			Paint paint;
 			final float verticalPitchScale = (float) (75./20); // 75pixels/20 kts = 3.75 pixels/kts
-			offsetx = centerx - 300; //Border
+			offsetx = centerx - (int)(300*scaleFactor); //Border
 			
 			paint = new Paint();
 			paint.setAntiAlias(true);
 			paint.setDither(true);
 			paint.setColor(Color.WHITE);
-			paint.setTextSize(30);
+			paint.setTextSize(30*scaleFactor);
 			paint.setStrokeWidth(2);
 			
 			//Calculate first horizontal line
 			int rest = (int)(speed)%20; //residual to 1st lower mark in kts
 			int speed1 = (int)(speed) - rest; // speed in kts of 1st lower mark
-			int offset1 = (int) (rest*verticalPitchScale);
+			int offset1 = (int) (rest*verticalPitchScale*scaleFactor);
 			int y1 = centery + offset1;
 			
 			int y[] = new int[8];
@@ -240,17 +233,17 @@ public class MFD777View extends SurfaceView implements SurfaceHolder.Callback {
 			
 			//Marks with text
 			for (int i = 0; i<8; i++){
-				y[i] = y1 - (75*4) + i*75;
+				y[i] = y1 - (int)(((75*4) - i*75)*scaleFactor);
 				alt[i] = speed1 + (4*20) - i*20;
-				canvas.drawLine(offsetx, y[i], offsetx+30, y[i], paint);
-				canvas.drawText(String.format("%d", alt[i]), offsetx - 60, y[i]+10, paint);
+				canvas.drawLine(offsetx, y[i], offsetx+(int)(30*scaleFactor), y[i], paint);
+				canvas.drawText(String.format("%d", alt[i]), offsetx - (int)(60*scaleFactor), y[i]+(int)(10*scaleFactor), paint);
 				
 			}
 			
 			//Marks without text
 			for (int i = 0; i<8; i++){
-				y[i] = y1 - (75*4+37) + i*75; 
-				canvas.drawLine(offsetx, y[i], offsetx+30, y[i], paint);			
+				y[i] = y1 - (int)(((75*4+37) - i*75)*scaleFactor); 
+				canvas.drawLine(offsetx, y[i], offsetx+(int)(30*scaleFactor), y[i], paint);			
 			}		
 	}
 		
@@ -259,15 +252,15 @@ public class MFD777View extends SurfaceView implements SurfaceHolder.Callback {
 		int x1,x2,y1,y2;
 		Paint paint;
 		
-		float scaleVs = (float) (180./3000.); //180 pixels / 3000 kfps (Full-scale +-180 pixels)
+		float scaleVs = (float) (180./3000.); //180 pixels / 3000 kfpm (Full-scale +-180 pixels)
 				
 		//x1 = centerx + 475; //right border
-		x1 = centerx + 550; //right point
-		x2 = centerx + 435; //
+		x1 = centerx + (int)(550*scaleFactor); //right point
+		x2 = centerx + (int)(435*scaleFactor); //
 		y1 = centery;
 		
 		//Calculation of line deviation
-		y2 = centery - (int)(verticalSpeed*scaleVs);
+		y2 = centery - (int)(verticalSpeed*scaleVs*scaleFactor);
 		
 		paint = new Paint();
 		paint.setAntiAlias(true);
